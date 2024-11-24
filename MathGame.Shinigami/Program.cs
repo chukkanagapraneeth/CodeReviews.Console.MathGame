86498cd5f8aa8509dd5204f7ea2eb1eb602b3140
Shin <cnagapraneeth@gmail.com>
2024-11-24 23:17:53 +0530

    1st Project Finished!!!

---
+++ MathGame.Shinigami/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathGame game = new MathGame();
+            game.Menu();
+        }
+    }
+}
